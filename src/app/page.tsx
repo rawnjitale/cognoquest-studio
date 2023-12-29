2a7dab529dd56520d5ec9671c9ad41b266fae6ec
rawnjitale <rawnjit152@gmail.com>
2023-12-29 22:08:01 +0545

    Added prettierrc for tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex justify-center ">
-      <div className="md:max-w-[1200px] flex flex-col pt-4  w-full">
+      <div className="flex w-full flex-col pt-4  md:max-w-[1200px]">
         <UserButton afterSignOutUrl="\" />
         <div className="pt-5">
-          <h1 className="text-4xl text-center">Cognoquest Studio</h1>
+          <h1 className="text-center text-4xl">Cognoquest Studio</h1>
         </div>
         <div className="flex flex-row justify-center ">
-          <div className="w-full flex justify-center items-center">
+          <div className="flex w-full items-center justify-center">
             <Image
               src="/homepage-main.png"
               alt="Main page"
@@ -18,16 +19,18 @@ export default function Home() {
               height={500}
             />
           </div>
-          <p className="text-center text-xl pt-5 absolute">
+          <p className="absolute pt-5 text-center text-xl">
             Welcome to Cognoquest Studio <br />
             Cognoquest Studio is a platform for creating and sharing interactive
             learning experiences. <br />
             Click on Explore to get started
             <br />
           </p>
-          <button className="absolute bottom-10 right-1/3 py-3 px-4 bg-blue-500 rounded-xl text-2xl hover:bg-blue-800 hover:scale-90">
-            Explore
-          </button>
+          <Link href="/room">
+            <button className="absolute bottom-10 right-1/3 rounded-xl bg-blue-500 px-4 py-3 text-2xl hover:scale-90 hover:bg-blue-800">
+              Explore
+            </button>
+          </Link>
         </div>
       </div>
     </main>
